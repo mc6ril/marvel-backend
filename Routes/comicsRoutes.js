cf6d94f6e008d41d85abc4affd0b86acdcbb0b87
mc6ril <cyril.lesot@yahoo.fr>
2021-08-05 11:36:22 +0200

    ok

--- Routes/comicsRoutes.js
+++ Routes/comicsRoutes.js
@@ -4,30 +4,10 @@ const axios = require('axios');
 
 router.get('/comics', async (req, res) => {
     try {
-        const filter = {};
-
-        const api_key = process.env.MARVEL_API_KEY;
-
-        const { title, limit, apiKey } = req.query;
-
-        // filtre par titre
-        if (title) {
-            let result = new RegExp(title, 'i');
-            filter.title = result;
-        }
-
-        //Pagination
-        let page;
-        const newLimit = Number(limit);
-        if (Number(limit) < 1) {
-            page = 1;
-        } else {
-            page = Number(page);
-        }
-        const skip = limit * (page - 1);
+        const { title, skip } = req.query;
 
         const response = await axios.get(
-            `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}`,
+            `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}&skip=${skip}&title=${title}`,
         );
 
         console.log(response.data);
