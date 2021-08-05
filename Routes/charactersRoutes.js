f6b402bacffe8c6aa8efa7e4068cf84017fc4a6a
mc6ril <cyril.lesot@yahoo.fr>
2021-08-05 10:41:32 +0200

    ok

--- Routes/charactersRoutes.js
+++ Routes/charactersRoutes.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 router.get('/characters', async (req, res) => {
     try {
         const response = await axios.get(
-            `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}`,
+            `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}&skip=${req.query.skip}`,
         );
 
         console.log(response.data);
